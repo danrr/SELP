1b175eae0abe93178b3b1de45c29d8155e7ec1e4
Dan Ristea <s1244166@sms.ed.ac.uk>
2014-10-15 12:01:20 +0100

    Using python 2.7 in setting up virtualenv

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import subprocess
 
-subprocess.call("virtualenv env", shell=True)
+subprocess.call("virtualenv -p /usr/bin/python2.7 env", shell=True)
 
 subprocess.call("source env/bin/activate && pip install -r requirements.txt", shell=True)
