1d143d6ffc9d6237a1102a96a2bcb5d461dc0c9d
Dan Ristea <s1244166@sms.ed.ac.uk>
2014-11-27 16:16:15 +0000

    Adding User model with password hashing.
    User class inherits from SQLAlchemy model, and will be used to query the database for user details.

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from app import db
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(120))
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
