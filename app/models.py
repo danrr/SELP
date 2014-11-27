22724e85f65761e881cb8e17b0c378565523276e
Dan Ristea <s1244166@sms.ed.ac.uk>
2014-11-27 18:33:02 +0000

    Adding post db model.
    This will store the recipes that will show up on the website.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import datetime
 from app import db
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -7,6 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(120))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, username, email, password):
         self.username = username
@@ -31,3 +33,21 @@ class User(db.Model):
     def __repr__(self):
         return '<User {username}>'.format(username=self.username)
 
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.Text)
+    publish_time = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, body, user_id, publish_time=None):
+        self.title = title
+        self.body = body
+        self.user_id = user_id
+        if not publish_time:
+            publish_time = datetime.utcnow()
+        self.publish_time = publish_time
+
+    def __repr__(self):
+        return '<Post {title}>'.format(title=self.title)
