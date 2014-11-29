2cec42b9c8f9e10aa2503ea83f6db594c4c55faf
Dan Ristea <s1244166@sms.ed.ac.uk>
2014-11-29 12:53:24 +0000

    Adding base test from which other tests can inherit. It sets up and tears down all the requirements, and configures the test app. It also contains methods that make testing strings easier.

---
+++ tests/base_test.py
@@ -0,0 +1,24 @@
+#!flask/bin/python
+import unittest
+
+from app import app, db
+
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        app.config.from_object('tests.test_config')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def assert_contains_string(self, base, string):
+        self.assertTrue(base.find(string) > -1, msg="{string} not found in {base}".format(string=string, base=base))
+
+    def assert_does_not_contain_string(self, base, string):
+        self.assertFalse(base.find(string) > -1, msg="{string} found in {base}".format(string=string, base=base))
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_config.py
+++ tests/test_config.py
@@ -3,3 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+TESTING = True
+WTF_CSRF_ENABLED = False
