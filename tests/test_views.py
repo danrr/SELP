cd6aaba4b5c287cfc7095d8cad798cfcd6443d26
Dan Ristea <s1244166@sms.ed.ac.uk>
2014-12-07 12:55:21 +0000

    Fixing tests broken by change in HTML templates

--- tests/test_views.py
+++ tests/test_views.py
@@ -9,7 +9,7 @@ class TestViews(BaseTest):
     def test_homepage_displays_with_no_posts(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assert_contains_string(response.data, '<div class="navigation">')
+        self.assert_contains_string(response.data, '<nav role="navigation"')
         self.assert_does_not_contain_string(response.data, '<div class="post">')
 
     def test_homepage_displays_with_posts(self):
@@ -19,6 +19,6 @@ class TestViews(BaseTest):
         db.session.add(post)
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assert_contains_string(response.data, '<div class="navigation">')
+        self.assert_contains_string(response.data, '<nav role="navigation"')
         self.assert_contains_string(response.data, '<div class="post">')
         self.assert_contains_string(response.data, 'POSTBODY')
