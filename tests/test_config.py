48d65bd49b7d0b76e9faf067ba42506828af6e37
Dan Ristea <s1244166@sms.ed.ac.uk>
2014-12-09 01:09:58 +0000

    Configuring separate search.db for test. It was behaving weirdly after tests runs.

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,5 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
 TESTING = True
 WTF_CSRF_ENABLED = False
